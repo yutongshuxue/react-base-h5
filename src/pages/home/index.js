6e4f7bd34c24973cd2bc21bac66f9656073803d8
yutongshuxue <yutongshuxue@126.com>
2022-10-16 18:33:49 +0800

    init1.4 移动端1px

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,12 @@
 import Icon from "@/components/Icon"
+import styles from './index.module.scss'
 function Home(){
   return(
     <div>
       <div className="home">home pagesDSFWE</div>
         <Icon type='icon-anquanchaxun' style={{fontSize:100}} onClick={()=>alert('hahah')}/>
+
+        <div className={styles.test_border}></div>
     </div>
     
   )
